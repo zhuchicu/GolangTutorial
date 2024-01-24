fe06bd9f91f485a7554b9e550dee005f75c09805
Maolin Zhu <20656537+zhuchicu@users.noreply.github.com>
2024-01-24 11:00:39 +0800

    Update helloweb.go

--- 02-HelloWeb/helloweb.go
+++ 02-HelloWeb/helloweb.go
@@ -1,12 +1,12 @@
 package main
 
-import (
+import (        // 嵌套结构导入
     "fmt"
-    "net/http"
+    "net/http"  // 官方 package
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {  // 匿名函数，可以直接调用，或赋值给一个变量
         fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
     })
 
